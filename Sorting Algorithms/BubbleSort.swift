c37fd67b3ce751bc9239793ff9275236a953ffd0
momonepal <49270582+momonepal@users.noreply.github.com>
2020-05-24 02:39:47 -0400

    Add files via upload

---
+++ Sorting Algorithms/BubbleSort.swift
@@ -0,0 +1,54 @@
+//  BubbleSort.swift
+//  Sorting
+//  Created by Mohit
+//  Copyright © 2020 Mohit. All rights reserved.
+
+import Foundation
+extension Array where Element: Comparable {
+    func BubbleSort() -> Array{
+        var array = self
+        for _ in array{
+            var swapped = false
+            for index in 1..<array.count{
+                if array[index-1] > array[index] {
+                    array.swapAt( index,index-1)
+                    swapped = true
+                }
+            }
+            if swapped == false{
+                break
+            }
+        }
+        return array
+    }
+    
+    func InsertionSort() -> Array {
+        var array = self
+        for index in 1..<array.count{
+            let temp = array[index]
+            var position = index
+            while array[position-1]>temp && position>0 {
+                array[position] = array[position-1]
+                position -= 1
+            }
+            array[position] = temp
+        }
+        return array
+    }
+    
+    func SelectionSort() -> Array {
+        var array = self
+        for idx in 0..<array.count{
+            var lowindex = idx
+            for index in idx+1..<array.count{
+                if array[index] < array[lowindex] {
+                    lowindex = index
+                }
+            }
+            array.swapAt(lowindex, idx)
+        }
+        return array
+    }
+    
+    
+}
