f67621a6d55e607a673f01757de47522b9fecada
momonepal <49270582+momonepal@users.noreply.github.com>
2020-05-27 11:07:57 -0400

    Add files via upload

--- Graphs/graph.swift
+++ Graphs/graph.swift
@@ -26,6 +26,57 @@ class Graph {
         }
        adj[firstVertex]?.append(secondVertex)
        adj[secondVertex]?.append(firstVertex)
+        
+       adj.keys.sorted()  // ??
     }
+    
+    func removeEdge( firstVertex : Int, secondVertex : Int ){
+        if adj[firstVertex] == nil || adj[secondVertex] == nil {
+        }
+        else{
+            if (adj[firstVertex]?.contains(secondVertex))! {
+                adj[firstVertex] = adj[firstVertex]?.filter {$0 != secondVertex}
+
+                adj[secondVertex] = adj[secondVertex]?.filter {$0 != firstVertex}
+            }
+        }
+    }
+    
+    func neighborsOf( vertex : Int ) -> [Int]{
+        return adj[vertex]!
+    }
+    
+    func verticesAreAdjacent( firstVertex : Int, secondVertex : Int ) -> Bool{
+        if (adj[firstVertex]?.contains(secondVertex))! {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    func verticesAreConnected(firstVertex : Int, secondVertex : Int) -> Bool{
+        if self.verticesAreAdjacent(firstVertex: firstVertex, secondVertex: secondVertex) {
+            return true
+        }
+        else{
+            
+        }
+        return false
+        
+        
+    }
+    
+    
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
