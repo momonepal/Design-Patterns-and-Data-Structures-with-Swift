1cb51159a5a60732ce6c53db0168b507065a0f34
Mohit <49270582+momonepal@users.noreply.github.com>
2020-05-29 03:29:34 -0400

    Add files via upload

---
+++ RedBlackTree/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  rbt
+//
+//  Created by Mohit on 5/28/20.
+//  Copyright © 2020 Mohit. All rights reserved.
+//
+
+import Foundation
+
+let a = RedBlackTree<Int>()
+a.insert(element: 4)
+a.insert(element: 7)
+a.insert(element: 1)
+a.insert(element: 8)
+a.insert(element: 3)//balanced upto here
+a.insert(element: 6)
+a.insert(element: 10)
+a.insert(element: 2)
+a.insert(element: 9)
+
+print(a.makeBreadthFirstArray())
+print(a.height)
+print(a.size)
+
